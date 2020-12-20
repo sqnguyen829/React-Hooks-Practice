0d4e61e9e7b252732f13f90ede0173274a06f8f6
sqnguyen829 <sqnguyen829@gmail.com>
2020-12-20 16:34:31 -0600

    added isMounted to clean up any warning flags from the console

--- src/Containers/DogList.js
+++ src/Containers/DogList.js
@@ -10,13 +10,18 @@ export default function DogList() {
     //useEffect() is similar to componentDidMount() but componentDidMount only runs once, and to get the same effect is 
     //componentDidMount we pass an empty array as the secound argument
     useEffect(() => {
+        let isMounted = true // note this flag denote mount status
+
         fetch('http://localhost:3000/dogs')
         .then(res => res.json())
         .then(dogsData => {
-            setDog(dogsData)
+            if(isMounted){
+                setDog(dogsData)
+            }
         })
-        //Uncommenting this next line does some interesting things in the inspect
-        setCounter(counter + 1)
+        //Uncommenting this next line does some interesting things in the console
+        // setCounter(counter + 1)
+        return () => { isMounted = false } // use effect cleanup to set flag false, if unmounted
     },[])
     //we could put varibles in the array and whenever 
     //the varibles in the array is changed it will run the useEffect
